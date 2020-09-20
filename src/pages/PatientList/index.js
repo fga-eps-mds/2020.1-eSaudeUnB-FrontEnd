cd6d1fa144950000f4bbd5fc6daa17500e872c5f
Caio Martins <caio@wgo.com.br>
2020-09-20 20:04:57 -0300

    fix: size of some components

--- src/pages/PatientList/index.js
+++ src/pages/PatientList/index.js
@@ -1,62 +1,56 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
-import api from '../../services/api';
-import './styles.css';
+import api from "../../services/api";
+import "./styles.css";
 
-import MagnifyingGlass from '../../assets/images/lupa.svg';
-import go from '../../assets/images/go.svg';
+import MagnifyingGlass from "../../assets/images/lupa.svg";
+import go from "../../assets/images/go.svg";
 
-import userIcon from '../../assets/images/userIcon.svg';
+import userIcon from "../../assets/images/userIcon.svg";
 
-import NavBar from '../../components/NavBar';
-import SearchBar from '../../components/SearchBar';
+import NavBar from "../../components/NavBar";
+import SearchBar from "../../components/SearchBar";
 
 export default function PatientList() {
-    const [search, setSearch] = useState('');
-    const [patients, setPatients] = useState([]);
+  const [search, setSearch] = useState("");
+  const [patients, setPatients] = useState([]);
 
-    useEffect(() => {
-        api.get('/users')
-            .then((response) => {
-                setPatients(response.data);
-            });
-    }, []);
+  useEffect(() => {
+    api.get("/users").then((response) => {
+      setPatients(response.data);
+    });
+  }, []);
 
-    return (
-        <div className="patientListContainer">
-            <div className="content">
-                <NavBar className="navBar" />
+  return (
+    <div className="patientListContainer">
+      <div className="content">
+        <NavBar className="navBar" />
+        <SearchBar
+          className="searchBar"
+          value={search}
+          onChange={setSearch}
+          icon={MagnifyingGlass}
+        />
 
-                <SearchBar
-                    className="searchBar"
-                    value={search}
-                    onChange={setSearch}
-                    icon={MagnifyingGlass}
-                />
-
-                <div className="patients">
-                    {patients.map((patient) => (
-                        <div key={patient.email} className="patientTab">
-                            <div className="patientInfos">
-                                <img className="patientImg" src={userIcon} alt={patient.name} />
-                                <div className="minPatient">
-                                    <p>{`${patient.name} ${patient.lastName}`}</p>
-                                    <p>{patient.email}</p>
-                                </div>
-                            </div>
-
-                            <Link
-                                className="button"
-                                to={`patient-list/${patient.id}`}
-                            >
-                                <img src={go} alt="go" />{' '}
-                            </Link>
-                        </div>
-
-                    ))}
+        <div className="patients">
+          {patients.map((patient) => (
+            <div key={patient.email} className="patientTab">
+              <div className="patientInfos">
+                <img className="patientImg" src={userIcon} alt={patient.name} />
+                <div className="minPatient">
+                  <p>{`${patient.name} ${patient.lastName}`}</p>
+                  <p>{patient.email}</p>
                 </div>
+              </div>
+
+              <Link className="button" to={`patient-list/${patient.id}`}>
+                <img src={go} alt="go" />{" "}
+              </Link>
             </div>
+          ))}
         </div>
-    );
+      </div>
+    </div>
+  );
 }
