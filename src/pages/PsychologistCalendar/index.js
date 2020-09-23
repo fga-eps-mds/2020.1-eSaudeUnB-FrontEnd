c37abe3008649cd0102b9a643c6992ac92a53efd
Caio Martins <caio@wgo.com.br>
2020-09-23 18:19:51 -0300

    fix: edit PsychologistCalendar page style

--- src/pages/PsychologistCalendar/index.js
+++ src/pages/PsychologistCalendar/index.js
@@ -1,37 +1,36 @@
-import React, { useState } from "react";
-import { useHistory } from "react-router-dom";
-import Calendar from "react-calendar";
-import api from "../../services/api";
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import Calendar from 'react-calendar';
+import api from '../../services/api';
 
-import "./styles.css";
-import NavBar from "../../components/NavBar";
+import './styles.css';
+import NavBar from '../../components/NavBar';
 
 export default function PsychologistCalendar() {
-  const [date, setDate] = useState(new Date());
-  return (
-    <div className="psychologistcalendar">
-      <div className="content">
-          <NavBar className="navBar" />
-        <div className="tabela">
-        <div className="calendar">
-          <Calendar
-            onChange={(currentDate) => {
-              setDate(currentDate);
-            }}
-            value={date}
-            next2Label={null}
-            prev2Label={null}
-          />
-        </div>
-        <div className="table-right">
-          <h1>Seus Atendimentos no Dia:</h1>
-            <div className="atendimento">
-                <p>Atendimento 1 </p>
-
+    const [date, setDate] = useState(new Date());
+    return (
+        <div className="psychologistcalendar">
+            <NavBar className="navBar" />
+            <div className="content">
+                <div className="tabela">
+                    <div className="calendar">
+                        <Calendar
+                            onChange={(currentDate) => {
+                                setDate(currentDate);
+                            }}
+                            value={date}
+                            next2Label={null}
+                            prev2Label={null}
+                        />
+                    </div>
+                    <div className="table-right">
+                        <h1>Horários de atendimento:</h1>
+                        <div className="atendimento">
+                            <p>Atendimento 1 </p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-      </div>
-      </div>
-    </div>
-  );
+    );
 }
