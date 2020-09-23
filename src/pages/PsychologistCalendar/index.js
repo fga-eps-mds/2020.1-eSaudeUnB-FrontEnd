eb67ade5b219692d27b878db4fdfb4cc63a08c18
Luis Marques <luisgustavomarques@outlook.com>
2020-09-23 17:18:35 -0300

    feat:PsychologistCalendar

--- src/pages/PsychologistCalendar/index.js
+++ src/pages/PsychologistCalendar/index.js
@@ -1,23 +1,37 @@
-// import React, { useState } from 'react';
-// import Calendar from 'react-calendar';
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import Calendar from "react-calendar";
+import api from "../../services/api";
 
-// export default function psychologistCalendar() {
-//     const [date, setDate] = useState(new Date());
-//     return (
-//         return(<div className="baixo">
-//             <div className="calendar">
-//                 <Calendar
-//                     onChange={(currentDate) => {
-//                         setDate(currentDate);
-//                     }}
-//                     value={date}
-//                     next2Label={null}
-//                     prev2Label={null}
-//                 />
-//             </div>
-//             <div className="schedule">
-//                 <p>Seus Atendimentos no Dia:</p>
-//             </div>
-//         </div>
-//     );
-// }
+import "./styles.css";
+import NavBar from "../../components/NavBar";
+
+export default function PsychologistCalendar() {
+  const [date, setDate] = useState(new Date());
+  return (
+    <div className="psychologistcalendar">
+      <div className="content">
+          <NavBar className="navBar" />
+        <div className="tabela">
+        <div className="calendar">
+          <Calendar
+            onChange={(currentDate) => {
+              setDate(currentDate);
+            }}
+            value={date}
+            next2Label={null}
+            prev2Label={null}
+          />
+        </div>
+        <div className="table-right">
+          <h1>Seus Atendimentos no Dia:</h1>
+            <div className="atendimento">
+                <p>Atendimento 1 </p>
+
+            </div>
+        </div>
+      </div>
+      </div>
+    </div>
+  );
+}
