8528b219ca4c6c61b0e89a1c915ea39d2cfed206
Rafaelltm <rafaelltm10@hotmail.com>
2020-09-23 11:51:49 -0300

    fix: fix alerts on landingLogin page, and fix psyList design
    Co-authored-by: abner423 <abner.f.c.r@hotmail.com>

--- src/pages/LandingLogin/index.js
+++ src/pages/LandingLogin/index.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
+import { Alert } from 'react-bootstrap';
 
 import api from '../../services/api';
 import './styles.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import userIcon from '../../assets/images/userIcon.svg';
 import Union from '../../assets/images/Union.svg';
@@ -14,6 +16,9 @@ import Input from '../../components/Input';
 export default function LandingLogin() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [show, setShow] = useState(false);
+    const [alertText, setAlertText] = useState('');
+    const [variant, setVariant] = useState('');
 
     const history = useHistory();
 
@@ -22,6 +27,18 @@ export default function LandingLogin() {
             event.preventDefault();
 
             const responseUser = await api.post('/loginUser', { email, password });
+
+            if (responseUser.status === 404 || responseUser.status === 400) {
+                setShow(true);
+                setVariant('danger');
+                setAlertText('Email/Senha incorretos, digite novamente.');
+            }
+
+            if (responseUser.status === 500) {
+                setShow(true);
+                setVariant('danger');
+                setAlertText('Ocorreu algum erro no login, tente novamente.');
+            }
 
             if (responseUser.status === 200 || responseUser.status === 201) {
                 alert('Login efetuado');
@@ -45,19 +62,27 @@ export default function LandingLogin() {
                 }
 
                 if (err.response.status === 404 || err.response.status === 400) {
-                    alert('Email/Senha incorretos, tente novamente.');
+                    setShow(true);
+                    setVariant('danger');
+                    setAlertText('Email/Senha incorretos, digite novamente.');
                 }
 
                 if (err.response.status === 500) {
-                    alert('Ocorreu algum erro no seu login, tente novamente');
+                    setShow(true);
+                    setVariant('danger');
+                    setAlertText('Ocorreu algum erro no seu login, tente novamente.');
                 }
             } catch (err2) {
                 if (err2.response.status === 404 || err2.response.status === 400) {
-                    alert('Email/Senha incorretos, tente novamente.');
+                    setShow(true);
+                    setVariant('danger');
+                    setAlertText('Email/Senha incorretos, digite novamente.');
                 }
 
                 if (err2.response.status === 500) {
-                    alert('Ocorreu algum erro no seu login, tente novamente');
+                    setShow(true);
+                    setVariant('danger');
+                    setAlertText('Ocorreu algum erro no seu login, tente novamente.');
                 }
             }
         }
@@ -65,11 +90,17 @@ export default function LandingLogin() {
 
     return (
         <div className="loginContainer">
+            {show ? (
+                <Alert className="alert" variant='danger' onClose={() => setShow(false)} dismissible>{alertText}</Alert>
+            ) : (
+                    <div></div>
+                )}
             <div className="content">
                 <Logo />
 
                 <form className="form" onSubmit={handleLogin}>
                     <img className="userIcon" src={userIcon} alt="icone de usuario" />
+
                     <Input
                         placeholder="Email"
                         value={email}
