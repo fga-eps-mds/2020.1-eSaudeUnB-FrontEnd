c9c06eb93289c00985e985cd5554f7a81a87f7c4
Rafaelltm <rafaelltm10@hotmail.com>
2020-09-24 09:58:41 -0300

    fix: fix warnings on login page

--- src/pages/LandingLogin/index.js
+++ src/pages/LandingLogin/index.js
@@ -40,7 +40,7 @@ export default function LandingLogin() {
 
             if (err.response.status === 404 || err.response.status === 400) {
                 setShow(true);
-                setVariant('danger');
+                setVariant('warning');
                 setAlertText('Email/Senha incorretos.');
             }
 
@@ -50,13 +50,17 @@ export default function LandingLogin() {
                 setAlertText('Ocorreu algum erro no login, tente novamente.');
             }
 
+            setInterval(() => {
+                setShow(false);
+            }, 4000);
+
         }
     }
 
     return (
         <div className="loginContainer">
             {show ? (
-                <Alert className="alert" variant='danger' onClose={() => setShow(false)} dismissible>{alertText}</Alert>
+                <Alert className="alert" variant={variant}>{alertText}</Alert>
             ) : (
                     <div></div>
                 )}
