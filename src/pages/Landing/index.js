f572adb348a0f3b10d80f7eef0b7da6d83ba6b90
Rafaelltm <rafaelltm10@hotmail.com>
2020-10-21 00:36:47 -0300

    feature: initial structure of new landing page
    Co-authored-by: PerhapsAlex <caio@wgo.com.br>

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -1,30 +1,46 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import LandingNavBar from '../../components/LandingNavBar';
+
 import landingImg from '../../assets/images/logo.svg';
 
 import './styles.css';
 
 export default function Landing() {
     return (
-        <div id="page-landing">
-            <div id="page-landing-content" className="container">
-                <img src={landingImg} alt="e-Saúde" className="hero-image" />
+        <div className="page-landing">
+            <LandingNavBar className="nav-bar" />
+            <div className="content">
+                <div className="main">
+                    <img className="landing-img" src={landingImg} />
+                    <div className="above-img">
+                        <div class="logo-container">
+                            <h1>e-Saúde</h1>
+                            <h2>Mesmo à distância nós cuidamos de você</h2>
+                        </div>
 
-                <div className="logo-container">
-                    <h1>e-Saúde</h1>
-                    <h2>Mesmo à distância nós cuidamos de você</h2>
+                        <div className="buttons-container">
+                            <Link to="/registration" className="registration">
+                                Registrar
+                            </Link>
+
+                            <Link to="/login" className="login">
+                                Entrar
+                            </Link>
+                        </div>
+                    </div>
+                    <section className="wave-animation">
+                        <div className="wave wave1"></div>
+                        <div className="wave wave2" ></div>
+                        <div className="wave wave3" ></div>
+                    </section>
                 </div>
-
-                <div className="buttons-container">
-                    <Link to="/registration" className="registration">
-                        Registrar
-                    </Link>
-
-                    <Link to="/login" className="login">
-                        Entrar
-                    </Link>
-                </div>
+                <div className="about"></div>
+                <div className="how-it-work"></div>
+                <div className="faq"></div>
+                <div className="who-behind"></div>
+                <footer></footer>
             </div>
         </div>
     );
