3b9aa245e4677b709382b0702bae9f0912ad4998
unknown <abner.f.c.r@hotmail.com>
2020-11-02 04:25:42 -0300

    feat: add structure radio buttons

--- src/pages/UserMain/index.js
+++ src/pages/UserMain/index.js
@@ -13,6 +13,7 @@ export default function UserMain() {
     const [psychologists, setPsychologists] = useState([]);
     const [userSelected, setUserSelected] = useState({});
     const [show, setShow] = useState(false);
+    let anyDate = false;
 
     useEffect(() => {
         api.get('/psychologists').then((response) => {
@@ -29,109 +30,97 @@ export default function UserMain() {
         }
     }
 
-    async function checkAnyDate() {
-        let anyDate = false;
-        psychologists.map((psychologist) => {
-            psychologist.weekDay.map((workDay) => {
-                if (workDay.weekDay === date.getDay()) {
-                    anyDate = true;
-                }
-            });
-        });
-
-    }
-    function teste(psychologist) {
-        console.log(psychologist)
-        setUserSelected(psychologist)
-    }
     return (
         <div className="usercalendar">
             <NavBar className="navBar" bond="Patient" actualUser={'user'} />
             <div className="content">
-                    <div className="tabela">
-                        <div className="calendar">
-                            <Calendar
-                                onChange={ (currentDate) => {
-                                    setDate(currentDate);
-                                    checkAnyDate()
-                                    setUserSelected("")
-                                }}
-                                value={date}
-                                next2Label={null}
-                                prev2Label={null}
-                            />
-                        </div>
-                        <div className="table-right">
-                            <h1>{`Horários disponíveis em ${date.getDate()}/${date.getMonth() + 1}`}</h1>
-                            <div className="schedules">
-                                {psychologists.map((psychologist, index) => (
-                                    <div
-                                        // eslint-disable-next-line no-underscore-dangle
-                                        key={index}
-                                        className="schedule-box"
-                                    >
-                                        {psychologist.weekDay.map((workDay, index) => (
-                                            dateCheck(workDay.weekDay) ?
-                                                <div className="testecalendar" key={index}>
-                                                    
-                                                    <div className="psy-card"
-                                                        // eslint-disable-next-line no-underscore-dangle
-                                                        key={index}
-
-                                                    >
-                                                            <button onClick={() => teste(psychologist)}>
-                                                                <h3>Profissional: {psychologist.name} {psychologist.lastName}</h3>
-                                                            </button>
-
-                                                    </div>
-                                                </div>
-                                                : <div key={index}></div>
-                                        ))}
-                                    </div>
-                                ))}
-                            </div>
-                        </div>
-
+                <div className="tabela">
+                    <div className="calendar">
+                        <Calendar
+                            onChange={(currentDate) => {
+                                setDate(currentDate);
+                                setUserSelected("")
+                            }}
+                            value={date}
+                            next2Label={null}
+                            prev2Label={null}
+                        />
                     </div>
-                        {userSelected.weekDay !== undefined ?
-                        <div className="dropDown-calendar">
-                            <div className="column1">
-                                <h3>{userSelected.name} {userSelected.lastName}</h3>
-                                <h3>{userSelected.biography}</h3>
-                            </div>
-                            <div className="column2">
-                                <h3>Horários Disponíveis:</h3>
-                                <div className = "hours-disponibility">
+                    <div className="table-right">
+                        <h1>{`Horários disponíveis em ${date.getDate()}/${date.getMonth() + 1}`}</h1>
+                        <div className="schedules">
+                            {psychologists.map((psychologist, index) => (
+                                <div
+                                    // eslint-disable-next-line no-underscore-dangle
+                                    key={index}
+                                    className="schedule-box"
+                                >
+                                    {psychologist.weekDay.map((workDay, index) => (
+                                        dateCheck(workDay.weekDay) ?
+                                            <div className="testecalendar" key={index}>
+                                                {show ? setShow(false) : ""}
+                                                <div className="psy-card"
+                                                    // eslint-disable-next-line no-underscore-dangle
+                                                    key={index}
+
+                                                >
+                                                    <button onClick={() => setUserSelected(psychologist)}>
+                                                        <h3>Profissional: {psychologist.name} {psychologist.lastName}</h3>
+                                                    </button>
+
+                                                </div>
+                                            </div>
+                                            : <div key={index}>{!show ? setShow(true) : ""}</div>
+                                    ))}
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+
+                </div>
+                {userSelected.weekDay !== undefined ?
+                    <div className="dropDown-calendar">
+                        <div className="column1">
+                            <h3>{userSelected.name} {userSelected.lastName}</h3>
+                            <h3>{userSelected.biography}</h3>
+                        </div>
+                        <div className="column2">
+                            <h3>Horários Disponíveis:</h3>
+                            <form>
+                                <div className="hours-disponibility">
                                     {userSelected.weekDay !== undefined
                                         ? userSelected.weekDay.map((workDay) => (
                                             workDay.appointment.map((appointment) => (
-                                                
-                                                appointment.scheduled === false ?
-                                                <option value={appointment._id}>
-                                                    {appointment.time}
-                                                </option>
-                                                :
+
+                                                appointment.scheduled === false ? (
+                                                    <label >
+                                                        <input type="radio" name="hour" id ={appointment._id}  value={appointment.time} />
+                                                        {appointment.time}
+                                                    </label>
+                                                )
+                                                    :
                                                     ""
                                             ))
                                         ))
                                         : <div></div>
                                     }
                                 </div>
-                                <div className = "schedule-buttons">
-                                <button>Agendar</button>
-                                <button className = "cancelSchedule" onClick={() => setUserSelected("")}>Cancelar</button>
+                                <div className="schedule-buttons">
+                                    <button>Agendar</button>
+                                    <button className="cancelSchedule" onClick={() => setUserSelected("")}>Cancelar</button>
                                 </div>
-                            </div>
-                        </div> : 
-                        show ?
-                        <div>
+                            </form>
+                        </div>
+                    </div> :
+                    show ?
+                        <div className="noHours">
                             <h3>
-                                Nao possuimos horarios disponiveis
+                                Desculpe, não temos horários disponíveis em {date.getDate()}/{date.getMonth() + 1}
                             </h3>
                         </div>
                         : <div></div>
-                        
-                    }
+
+                }
             </div>
         </div >
     );
