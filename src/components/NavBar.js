424a006938405274a0ebf0cfb3d8050bfef1daed
Caio Martins <caio@wgo.com.br>
2020-11-05 20:34:53 -0300

    fix: navBar design
    Co-authored-by: pedroholiveira1998 <pdr_castro@hotmail.com>

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,39 +3,73 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import api from '../services/api';
 import userIcon from '../assets/images/userIcon.svg';
+import arrow from '../assets/images/up.svg';
 import logoQuadrado from '../assets/images/esaude_logo.svg';
 import '../assets/styles/NavBar.css';
 
 export default function NavBar({ actualUser, bond }) {
     const [userImage, setUserImage] = useState('');
+<<<<<<< HEAD
     const accessToken = localStorage.getItem('accessToken');
     const user = localStorage.getItem('user');
+=======
+    const [dropDown, setDropDown] = useState(false);
+
+>>>>>>> fix: navBar design
     useEffect(() => {
         (async function renderImage() {
             try {
                 if (bond === 'Psychologist') {
+<<<<<<< HEAD
                     const response = await api.get(`/psychologist/${user}`, {
                         headers: { authorization: accessToken },
                     });
+=======
+                    const response = await api.get(
+                        `/psychologist/${actualUser.email}`,
+                    );
+>>>>>>> fix: navBar design
 
-                    setUserImage(atob(Buffer.from(response.data.userImage, 'binary').toString('base64')));
+                    setUserImage(
+                        atob(
+                            Buffer.from(
+                                response.data.userImage,
+                                'binary',
+                            ).toString('base64'),
+                        ),
+                    );
                 } else {
                     const response = await api.get(`/user/${user}`, {
                         headers: { authorization: accessToken },
                     });
 
-                    setUserImage(atob(Buffer.from(response.data.userImage, 'binary').toString('base64')));
+                    setUserImage(
+                        atob(
+                            Buffer.from(
+                                response.data.userImage,
+                                'binary',
+                            ).toString('base64'),
+                        ),
+                    );
                 }
             } catch (err) {
                 // Erro ao renderizar imagem
             }
-        }());
+        })();
     }, [actualUser, bond]);
+
+    function showDropDown() {
+        setDropDown(!dropDown ? true : false);
+    }
 
     return (
         <nav className="navBarComponent">
             <div className="logo">
-                <img className="logoQuadrado" src={logoQuadrado} alt="icone de usuario" />
+                <img
+                    className="logoQuadrado"
+                    src={logoQuadrado}
+                    alt="icone de usuario"
+                />
             </div>
             {bond === 'Psychologist' ? (
                 <div className="navLinks">
@@ -74,9 +108,14 @@ export default function NavBar({ actualUser, bond }) {
                         Perfil
                     </Link>
 
-                    <img className="userIcon" src={userImage || userIcon} alt="icone de usuario" />
-                </div>)
-                : (<div className="navLinks">
+                    <img
+                        className="userIcon"
+                        src={userImage || userIcon}
+                        alt="icone de usuario"
+                    />
+                </div>
+            ) : (
+                <div className="navLinks">
                     {/* <Link className="a" to="" >Próximos Eventos</Link> */}
                     <Link
                         className="a"
@@ -122,10 +161,37 @@ export default function NavBar({ actualUser, bond }) {
                     >
                         Consultas Marcadas
                     </Link>
-                    <img className="userIcon" src={userImage || userIcon} alt="icone de usuario" />
+                   
+                    <div className="dropdown">
+                        <div className="images">
+                            <img
+                                className="userIcon"
+                                src={userImage || userIcon}
+                                alt="icone de usuario"
+                            />
+                            <img
+                                className="arrow"
+                                src={arrow}
+                                alt="menu"
+                                onClick={() => showDropDown()}
+                            />
+                        </div>
+                        {dropDown ? (
+                            <ul className="dropdown-items">
+                                <li>
+                                    <a href="">Perfil</a>
+                                </li>
+                                <li>
+                                    <a href="">Sair</a>
+                                </li>
+                            </ul>
+                        ) : (
+                            <div></div>
+                        )}
+                    </div>
                 </div>
-                )}
-        </nav >
+            )}
+        </nav>
     );
 }
 
