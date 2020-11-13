2cda271a6503c1e8ef977d49fcca3b59db6f6da2
Pedro Henrique <pdr_castro@hotmail.com>
2020-11-12 21:38:24 -0300

    feat: create responsive designer in Login page
    Co-authored-by: PerhapsAlex <caio@wgo.com.br>

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -4,11 +4,11 @@ import logo from '../assets/images/logo.svg';
 
 export default function Logo() {
     return (
-        <section className="section">
+        <section className="section logoImg">
             <img src={logo} alt="logo-eSaude" />
 
-            <p>e-Saúde UnB</p>
-            <p>Mesmo à distância nós cuidamos de você</p>
+            <p classaName="logoImg">e-Saúde UnB</p>
+            <p classaName="logoImg">Mesmo à distância nós cuidamos de você</p>
         </section>
     );
 }
