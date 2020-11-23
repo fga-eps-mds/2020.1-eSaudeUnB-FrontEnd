5617bcc2f29ac67233d616906a9ef5ce43564932
Vinicius <viniciusfa.delima@gmail.com>
2020-11-23 09:55:14 -0300

    fix: add scheduling to professional navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -82,6 +82,17 @@ export default function NavBar({ actualUser, bond }) {
                             >
                                 Agendamentos
                             </Link>
+                            <Link
+                                className="a"
+                                to={{
+                                    pathname: '/psychologist/schedule',
+                                    state: {
+                                        data: actualUser,
+                                    },
+                                }}
+                            >
+                                Horários
+                            </Link>
                         </div>
                     ) : (
                         <div className="navLinks">
