cf05ab24e2f7e03e35247bad873d5f636e1f7bd5
Vinicius <viniciusfa.delima@gmail.com>
2020-11-26 18:51:21 -0300

    feat: add scheduling to psy calendar page
    Co-authored-by: abner423 <abner.f.c.r@hotmail.com>

--- src/pages/PsychologistCalendar/index.js
+++ src/pages/PsychologistCalendar/index.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import Calendar from 'react-calendar';
 import api from '../../services/api';
 import './styles.css';
@@ -9,50 +10,54 @@ export default function PsychologistCalendar(props) {
     const [date, setDate] = useState(new Date());
     const [restricts, setRestricts] = useState([]);
     const accessToken = localStorage.getItem('accessToken');
+    const [scheduleItems, setScheduleItems] = useState([]);
+    const [show, setShow] = useState(false);
+    const [alertText, setAlertText] = useState('');
+    const [variant, setVariant] = useState('');
+    const user = localStorage.getItem('user');
 
-    async function putRestrict() {
-        await api.put('/calendary/update/', {
-            header: localStorage.getItem('acessToken'),
-            email: localStorage.getItem('user'),
-            restrict: [...restricts],
-        }, []);
-        window.location.reload();
-    }
+    const weekDays = [
+        {
+            value: 0,
+            label: 'Domingo',
+        },
+        {
+            value: 1,
+            label: 'Segunda-feira',
+        },
+        {
+            value: 2,
+            label: 'Terça-feira',
+        },
+        {
+            value: 3,
+            label: 'Quarta-feira',
+        },
+        {
+            value: 4,
+            label: 'Quinta-feira',
+        },
+        {
+            value: 5,
+            label: 'Sexta-feira',
+        },
+        {
+            value: 6,
+            label: 'Sábado',
+        },
+    ];
 
-    function checkRepeat() {
-        for (const item of restricts) {
-            if (
-                date.getDate() === item.day
-                && date.getMonth() === item.month
-                && date.getFullYear() === item.year
-            ) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    function updateRestricts() {
-        if (checkRepeat()) {
-            setRestricts([
-                ...restricts,
-                {
-                    year: date.getFullYear(),
-                    day: date.getDate(),
-                    month: date.getMonth(),
-                },
-            ]);
-        }
-    }
-
-    async function handleRestrict() {
-        const Restricts = await api.post('/calendary/restrict', {
-            email: localStorage.getItem('user'),
-        }, {
-            headers: { authorization: accessToken },
+    useEffect(() => {
+        api.post(
+            '/calendary/update',
+            {
+                email: user,
+            },
+            { headers: { authorization: accessToken } },
+        ).then((response) => {
+            setScheduleItems(response.data);
         });
-        setRestricts(Restricts.data);
-    }
+    }, []);
 
     function removeRestrict(index) {
         const temp = [...restricts];
@@ -60,8 +65,175 @@ export default function PsychologistCalendar(props) {
         setRestricts(temp);
     }
 
+    async function putCalendar(event) {
+        event.preventDefault();
+        if (verifyCalendarData()) {
+            await api.put(
+                '/calendary/update/',
+                {
+                    email: user,
+                    weekDay: scheduleItems,
+                },
+                {
+                    headers: { authorization: accessToken },
+                },
+            );
+            setShow(true);
+            setVariant('success');
+            setAlertText('Suas alterações foram salvas');
+            setInterval(() => {
+                setShow(false);
+            }, 3000);
+        }
+    }
+
+    function addNewScheduleItem() {
+        const ID = scheduleItems.length > 0 ? handleId(scheduleItems) : 0;
+        setScheduleItems([
+            ...scheduleItems,
+            {
+                weekDay: date.getDay(),
+                from: '',
+                to: '',
+                id: ID,
+                appointment: [],
+            },
+        ]);
+    }
+
+    function verifyCalendarData() {
+        let minutes;
+        for (let i = 0; i < scheduleItems.length; i++) {
+            if (scheduleItems[i].from > scheduleItems[i].to) {
+                setShow(true);
+                setVariant('danger');
+                setAlertText(
+                    'O horario de término não pode ser menor que o de ínicio',
+                );
+                setInterval(() => {
+                    setShow(false);
+                }, 3500);
+                return false;
+            }
+
+            if (!scheduleItems[i].from || !scheduleItems[i].to) {
+                setShow(true);
+                setVariant('danger');
+                setAlertText(
+                    'Todos os campos devem ser preenchidos, alterações não foram salvas',
+                );
+                setInterval(() => {
+                    setShow(false);
+                }, 3500);
+                return false;
+            }
+
+            if (scheduleItems[i].duration <= 0) {
+                setShow(true);
+                setVariant('danger');
+                setAlertText(
+                    'A duração da consulta deve ser maior que 0 minutos',
+                );
+                setInterval(() => {
+                    setShow(false);
+                }, 3500);
+                return false;
+            }
+            // function to be edited earlier
+            minutes = calculateAttendance(scheduleItems[i].from, scheduleItems[i].to, scheduleItems[i].duration);
+
+            if (minutes > 0) {
+                setShow(true);
+                setVariant('danger');
+                setAlertText(
+                    `Voce possui ${minutes} minutos que não se encaixarão em atendimentos`,
+                );
+                setInterval(() => {
+                    setShow(false);
+                }, 3500);
+                return false;
+            }
+            const value = appointmentHours(scheduleItems[i].from, scheduleItems[i].to, scheduleItems[i].duration);
+            scheduleItems[i].appointment = value;
+        }
+
+        return true;
+    }
+
+    function handleId() {
+        let newID = 0;
+        scheduleItems.forEach((item) => {
+            if (item.id >= newID) {
+                newID = item.id + 1;
+            }
+        });
+        return newID;
+    }
+
+    function calculateAttendance(start, end, duration) {
+        start = parseInt(start.substring(0, 2)) * 60 + parseInt(start.substring(3, 5));
+        end = parseInt(end.substring(0, 2)) * 60 + parseInt(end.substring(3, 5));
+        duration = parseInt(duration);
+
+        const number = (end - start);
+        let minutesRemaining = 0;
+        if (number % duration !== 0) {
+            minutesRemaining = number % duration;
+        }
+        return minutesRemaining;
+    }
+
+    function appointmentHours(start, end, duration) {
+        let actualHour = parseInt(start.substring(0, 2));
+        let actualMinutes = parseInt(start.substring(3, 5));
+        duration = parseInt(duration);
+        let hour = {};
+        const hours = [{}];
+        hours[0] = {
+            time: `${start}`,
+            scheduled: false,
+        };
+
+        do {
+            if (actualMinutes + duration >= 60) {
+                actualHour += 1;
+                actualMinutes = 60 - (actualMinutes + duration);
+            } else {
+                actualMinutes += duration;
+            }
+            hour = {
+                time: `${actualHour >= 10 ? actualHour : `0${actualHour}`}:${actualMinutes >= 10 ? actualMinutes : `0${actualMinutes}`}`,
+                scheduled: false,
+            };
+            if (hour.time !== end) {
+                hours.push(hour);
+            }
+        } while (hour.time !== end);
+        return hours;
+    }
+
+    function setScheduleItemsValue(position, field, value) {
+        const updatedScheduleItems = scheduleItems.map(
+            (scheduleItem, index) => {
+                if (index === position) {
+                    return { ...scheduleItem, [field]: value };
+                }
+
+                return scheduleItem;
+            },
+        );
+
+        setScheduleItems(updatedScheduleItems);
+    }
+
+    function removeScheduleItem(index) {
+        const temp = [...scheduleItems];
+        temp.splice(index, 1);
+        setScheduleItems(temp);
+    }
+
     return (
-        <div className="psychologistcalendar" onLoad={handleRestrict}>
+        <div className="psychologistcalendar">
             <NavBar
                 className="navBar"
                 bond="Psychologist"
@@ -81,32 +253,87 @@ export default function PsychologistCalendar(props) {
                     </div>
                     <div className="table-right">
                         <h1>Suas Restrições:</h1>
-                        <div className="schedules">
-                            {restricts.map((restrict, index) => (
+                        <form className="form" onSubmit={putCalendar}>
+                    <div className="formContent">
+                        <legend className="legend">
+                            Cadastrar horários disponíveis
+                            <button type="button" onClick={addNewScheduleItem}>
+                                + Novo Horário
+                            </button>
+                        </legend>
+
+                        <div className="schedule">
+                            {scheduleItems.map((scheduleItem, index) => (
                                 <div
-                                    key={index}
-                                    className="schedule-box"
+                                    key={scheduleItem._id || scheduleItem.id}
+                                    className="schedule-item"
                                 >
-                                    <span>{`restrição dia ${restrict.day}/${
-                                        restrict.month + 1 < 10
-                                            ? `0${restrict.month + 1}`
-                                            : `${restrict.month + 1}`
-                                    }/${restrict.year}`}</span>
+                                    <div className="input-box">
+                                        <label>Das</label>
+                                        <input
+                                            name="from"
+                                            label="Das"
+                                            type="time"
+                                            value={scheduleItem.from}
+                                            onChange={(e) => setScheduleItemsValue(
+                                                index,
+                                                'from',
+                                                e.target.value,
+                                            )
+                                            }
+                                        />
+                                    </div>
+
+                                    <div className="input-box">
+                                        <label>Até</label>
+                                        <input
+                                            name="to"
+                                            label="Até"
+                                            type="time"
+                                            value={scheduleItem.to}
+                                            onChange={(e) => setScheduleItemsValue(
+                                                index,
+                                                'to',
+                                                e.target.value,
+                                            )
+                                            }
+                                        />
+                                    </div>
+
+                                    <div className="input-box">
+                                        <label>Duração da consulta (minutos)</label>
+                                        <input
+                                            name="duration"
+                                            label="duration"
+                                            type="number"
+                                            min="0"
+                                            value={scheduleItem.duration}
+                                            onChange={(e) => setScheduleItemsValue(
+                                                index,
+                                                'duration',
+                                                e.target.value,
+                                            )
+                                            }
+                                        />
+                                    </div>
                                     <button
                                         type="button"
-                                        onClick={() => removeRestrict(index)}
+                                        onClick={() => removeScheduleItem(index)
+                                        }
                                     >
                                         Remover
                                     </button>
                                 </div>
                             ))}
                         </div>
-                        <button type="button" onClick={() => updateRestricts()}>
-                            {`Adicionar Restrição ao dia ${date.getDate()}`}
-                        </button>
-                        <button type="button" onClick={() => putRestrict()}>
-                            Salvar
-                        </button>
+
+                        <footer className="footer">
+                            <button type="submit">
+                                Salvar cadastro
+                            </button>
+                        </footer>
+                    </div>
+                </form>
                     </div>
                 </div>
             </div>
