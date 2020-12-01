710dd73d80f80b86a7bde37cb293b18fa0eb1406
Caio Martins <caio@wgo.com.br>
2020-12-01 20:40:00 -0300

    fix: add class to img
    Co-authored-by: pedroholiveira1998 <pdr_castro@hotmail.com>

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -5,7 +5,7 @@ import logo from '../assets/images/logo.svg';
 export default function Logo() {
     return (
         <section className="section logoImg">
-            <img src={logo} alt="logo-eSaude" />
+            <img className="logoImg" src={logo} alt="logo-eSaude" />
 
             <p className="logoImg">e-Saúde UnB</p>
             <p className="logoImg">Mesmo à distância nós cuidamos de você</p>
