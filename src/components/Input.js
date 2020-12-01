6bc9d3ee4502e4f66efc595eb2266a9fabeb239b
Daniel Goncalves <danmke@hotmail.com>
2020-12-01 00:31:02 -0300

    fix: correct params in input
    Signed-off-by: Daniel Goncalves <danmke@hotmail.com>

--- src/components/Input.js
+++ src/components/Input.js
@@ -2,11 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default function Input({
-    placeholder, value, onChange, type,
+    placeholder, value, onChange, type, className
 }) {
     return (
         <div className="input">
             <input
+                className={className}
                 placeholder={placeholder}
                 value={value}
                 onChange={(e) => onChange(e.target.value)}
