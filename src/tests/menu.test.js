c704351502ccbf1bd32cdf010c5d29fb415c648a
Vinicius <viniciusfa.delima@gmail.com>
2020-12-03 19:55:00 -0300

    feat: removed menu component
    Co-authored-by: Co-authored-by: pedroholiveira1998 <pdr_castro@hotmail.com>

--- src/tests/menu.test.js
+++
@@ -1,7 +0,0 @@
-import { shallow } from 'enzyme';
-import React from 'react';
-import Menu from '../components/Menu/Menu'
-
-it('Expect to render Input Component', () => {
-  expect(shallow(<Menu />).length).toEqual(1)
-})
