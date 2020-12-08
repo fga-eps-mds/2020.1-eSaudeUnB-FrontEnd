767f4d4a090d20ff2d3538338ce4b0b2c7c1a96d
Caio Martins <caio@wgo.com.br>
2020-12-08 00:24:12 -0300

    fix: default date on new-session

--- src/pages/NewSession/index.js
+++ src/pages/NewSession/index.js
@@ -13,17 +13,23 @@ export default function NewSession(props) {
     const [secondaryComplaint, setSecondaryComplaint] = useState('');
     const [complaintEvolution, setComplaintEvolution] = useState('');
     const [professional, setProfessional] = useState('');
-    const [date, SetDate] = useState('');
-    const [hour, SetHour] = useState('');
+
     const today = new Date();
+    const [date, SetDate] = useState(today.toISOString().slice(0, 10));
+    const [hour, SetHour] = useState(
+        `${today.getHours()}:${today.getMinutes()}`,
+    );
 
     useEffect(() => {
         async function getData() {
             const { email } = props.location.state;
             const accessToken = localStorage.getItem('accessToken');
-            const psy = await api.get(`/psychologist/${localStorage.getItem('user')}`, {
-                headers: { authorization: accessToken },
-            });
+            const psy = await api.get(
+                `/psychologist/${localStorage.getItem('user')}`,
+                {
+                    headers: { authorization: accessToken },
+                },
+            );
             const response = await api.get(`/user/${email}`, {
                 headers: { authorization: accessToken },
             });
@@ -41,18 +47,21 @@ export default function NewSession(props) {
 
         const { email } = patient;
         const accessToken = localStorage.getItem('accessToken');
-        const dia = Date.parse((`${date}T${hour}`));
-        await api.post('/session', {
-            email,
-            mainComplaint,
-            secondaryComplaint,
-            date: dia,
-            complaintEvolution,
-            professional: 'bbb',
-        },
-        {
-            headers: { authorization: accessToken },
-        });
+        const dia = Date.parse(`${date}T${hour}`);
+        await api.post(
+            '/session',
+            {
+                email,
+                mainComplaint,
+                secondaryComplaint,
+                date: dia,
+                complaintEvolution,
+                professional: 'bbb',
+            },
+            {
+                headers: { authorization: accessToken },
+            },
+        );
         history.push({
             pathname: `patient/list/${patient.email}`,
             state: {
@@ -71,17 +80,21 @@ export default function NewSession(props) {
                             {patient.userImage != null ? (
                                 <img
                                     className="patientImg"
-                                    src={atob(Buffer.from(patient.userImage, 'binary').toString('base64'))}
+                                    src={atob(
+                                        Buffer.from(
+                                            patient.userImage,
+                                            'binary',
+                                        ).toString('base64'),
+                                    )}
                                     alt={patient.name}
                                 />
-                            )
-                                : (
-                                    <img
-                                        className="patientImg"
-                                        src={userIcon}
-                                        alt={patient.name}
-                                    />
-                                )}
+                            ) : (
+                                <img
+                                    className="patientImg"
+                                    src={userIcon}
+                                    alt={patient.name}
+                                />
+                            )}
                             <div className="info">
                                 <div className="name">
                                     <span className="prop">Nome: </span>
@@ -96,31 +109,48 @@ export default function NewSession(props) {
 
                                     <div className="phone">
                                         <span className="prop">Telefone: </span>
-                                        <span>{patient.phone ? patient.phone : 'não informado'}</span>
+                                        <span>
+                                            {patient.phone
+                                                ? patient.phone
+                                                : 'não informado'}
+                                        </span>
                                     </div>
                                 </div>
 
                                 <div className="hidden">
                                     <span className="prop">Vinculo: </span>
-                                    <span>{patient.bond ? patient.bond : 'não informado'}</span>
+                                    <span>
+                                        {patient.bond
+                                            ? patient.bond
+                                            : 'não informado'}
+                                    </span>
 
                                     <span className="prop">Matricula: </span>
-                                    <span>{patient.unbRegistration ? patient.unbRegistration : 'não informado'}</span>
+                                    <span>
+                                        {patient.unbRegistration
+                                            ? patient.unbRegistration
+                                            : 'não informado'}
+                                    </span>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div className="patientHistory">
                         <div className="tab">
-                            <button id="voltar" className="tabLink"
+                            <button
+                                id="voltar"
+                                className="tabLink"
                                 onClick={() => history.push({
                                     pathname: `/patient/list/${patient.email}`,
                                     state: {
                                         email: patient.email,
                                         data: props.location.state.data,
                                     },
-                                })}
-                            >Voltar</button>
+                                })
+                                }
+                            >
+                                Voltar
+                            </button>
                         </div>
                         <form className="form" onSubmit={sendSession}>
                             <div className="tabContent">
@@ -128,61 +158,87 @@ export default function NewSession(props) {
                                     <div className="infos">
                                         <div className="professional">
                                             <p>Profissional:&nbsp;</p>
-                                            <span> {professional.name} {professional.lastName}</span>
-
+                                            <span>
+                                                {' '}
+                                                {professional.name}{' '}
+                                                {professional.lastName}
+                                            </span>
                                         </div>
 
                                         <div className="infoInputs">
-
                                             <div className="input-box">
                                                 <label>Data</label>
                                                 <input
                                                     type="date"
-                                                    defaultValue={today.toISOString().slice(0, 10)}
-                                                    onChange={(e) => SetDate(e.target.value)}
+                                                    defaultValue={today
+                                                        .toISOString()
+                                                        .slice(0, 10)}
+                                                    onChange={(e) => SetDate(e.target.value)
+                                                    }
                                                 />
                                             </div>
                                             <div className="input-box">
                                                 <label>Horário</label>
                                                 <input
                                                     type="time"
-                                                    defaultValue={`${new Date().getHours()}:${new Date().getMinutes()}`}
-                                                    onChange={(e) => SetHour(e.target.value)}
+                                                    defaultValue={`${today.getHours()}:${today.getMinutes()}`}
+                                                    onChange={(e) => SetHour(e.target.value)
+                                                    }
                                                 />
                                                 {/* 2020-01-29-12:50Z */}
                                             </div>
                                         </div>
-
                                         {/* <>Encaminhamento: SELECT</> */}
                                     </div>
 
-                                    <div className="recordText" id="mainComplaint" >
+                                    <div
+                                        className="recordText"
+                                        id="mainComplaint"
+                                    >
                                         <h1>Queixa Principal</h1>
                                         <textarea
-                                            onChange={(e) => setMainComplaint(e.target.value)}
+                                            onChange={(e) => setMainComplaint(e.target.value)
+                                            }
                                             type="mainComplaint"
                                         />
                                     </div>
 
-                                    <div className="recordText" id="secondaryComplaint">
+                                    <div
+                                        className="recordText"
+                                        id="secondaryComplaint"
+                                    >
                                         <h1>Queixa Secundaria</h1>
                                         <textarea
                                             value={secondaryComplaint}
-                                            onChange={(e) => setSecondaryComplaint(e.target.value)}
+                                            onChange={(e) => setSecondaryComplaint(
+                                                e.target.value,
+                                            )
+                                            }
                                             type="secondaryComplaint"
                                         />
                                     </div>
 
-                                    <div className="recordText" id="complaintEvolution">
+                                    <div
+                                        className="recordText"
+                                        id="complaintEvolution"
+                                    >
                                         <h1>Evolução das queixas</h1>
                                         <textarea
                                             value={complaintEvolution}
-                                            onChange={(e) => setComplaintEvolution(e.target.value)}
+                                            onChange={(e) => setComplaintEvolution(
+                                                e.target.value,
+                                            )
+                                            }
                                             type="complaintEvolution"
                                         />
                                     </div>
 
-                                    <button className="sendButton" type="submit">Enviar</button>
+                                    <button
+                                        className="sendButton"
+                                        type="submit"
+                                    >
+                                        Enviar
+                                    </button>
                                 </div>
                             </div>
                         </form>
