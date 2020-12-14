29665a7eee0c7a86fe69eeba2a6408255884a638
abner423 <abner.f.c.r@hotmail.com>
2020-12-14 01:25:23 -0300

    test: add test to cover profile patient
    Co-authored-by: Joberth Rogers <joberth.rogers18@gmail.com>

--- cypress/integration/patient/login.spec.js
+++ cypress/integration/patient/login.spec.js
@@ -327,4 +327,210 @@ describe("Login Patient User", () => {
     cy.visit("/events");
     cy.wait(3000);
   });
+
+  it("should return error when inputs are wrong", () => {
+    cy.loginPatient();
+
+    cy.get('#page1 > .form > .formRight > :nth-child(1) > .input > input').type('160128013');
+    cy.get('#page1 > .form > .formRight > :nth-child(3) > .input > input').type('61991257884');
+    cy.get(':nth-child(5) > .arrowButton').click();
+    cy.get('.formLeft > :nth-child(1) > .selectsLargest').select('Não possuo filha(o)/filhas(os)');
+    cy.get('.formLeft > :nth-child(2) > .selectsLargest').select('Solteiro');
+    cy.get('.formLeft > :nth-child(3) > .selectsLargest').select('Parda(o)');
+    cy.get('.formLeft > :nth-child(4) > .selectsLargest').select('Não');
+    cy.get(':nth-child(5) > .input > input').type('Não');
+    cy.get('.formRight > :nth-child(1) > .selectsLargest').select('Homem');
+    cy.get('#page2 > .form > .formRight > :nth-child(2) > .selectsLargest').select('Heterossexual');
+    cy.get('.formRight > :nth-child(3) > .selectsLargest').select('Não');
+    cy.get('.formRight > :nth-child(4) > .selectsLargest').select('Nunca fiz');
+    cy.get(':nth-child(5) > .selectsLargest').select('Ansiedade');
+    cy.get(':nth-child(5) > .arrowButton').click();
+    cy.get(':nth-child(4) > input').type('Arnaldo');
+    cy.get(':nth-child(8) > input').type('61991067698');
+    cy.get(':nth-child(12) > input').type('Primo');
+    cy.get('#page3 > .form > .formRight > :nth-child(1) > .input > input').type('Marlene Aparecida');
+    cy.get('.formRight > :nth-child(2) > .input > input').type('Antonio Rodrigues');
+    cy.get('#page3 > .form > .formRight > :nth-child(3) > .input > input').type('61991235124');
+    cy.get(':nth-child(5) > .arrowButton').click();
+
+    cy.get('.button-salvar').click();
+
+    cy.get('.fade').contains('Falha na atualização dos dados, tente novamente');
+    cy.wait(2000);
+  });
+
+  it("should return error when inputs are wrong", () => {
+    cy.route({
+      method: "PUT",
+      url: "**/user/**",
+      response: {
+        phone: null,
+        gender: null,
+        unbRegistration: null,
+        bond: "user",
+        civilStatus: null,
+        race: null,
+        sexualOrientation: null,
+        children: null,
+        emergencyContactName: null,
+        emergencyContactPhone: null,
+        emergencyContactBond: null,
+        motherName: null,
+        fatherName: null,
+        affiliationPhone: null,
+        socialPrograms: null,
+        studentHouseResidence: null,
+        psychiatricFollowUp: null,
+        medication: null,
+        mainComplaint: null,
+        sessions: [],
+        name: "teste",
+        lastName: "test",
+        email: "test@gmail.com",
+        ForgetPassword: 1,
+        appointments: [],
+        __v: 0,
+      },
+    });
+
+    cy.loginPatient();
+
+    cy.get('#page1 > .form > .formRight > :nth-child(1) > .input > input').type('160128013');
+    cy.get('#page1 > .form > .formRight > :nth-child(3) > .input > input').type('61991257884');
+    cy.get(':nth-child(5) > .arrowButton').click();
+    cy.get('.formLeft > :nth-child(1) > .selectsLargest').select('Não possuo filha(o)/filhas(os)');
+    cy.get('.formLeft > :nth-child(2) > .selectsLargest').select('Solteiro');
+    cy.get('.formLeft > :nth-child(3) > .selectsLargest').select('Parda(o)');
+    cy.get('.formLeft > :nth-child(4) > .selectsLargest').select('Não');
+    cy.get(':nth-child(5) > .input > input').type('Não');
+    cy.get('.formRight > :nth-child(1) > .selectsLargest').select('Homem');
+    cy.get('#page2 > .form > .formRight > :nth-child(2) > .selectsLargest').select('Heterossexual');
+    cy.get('.formRight > :nth-child(3) > .selectsLargest').select('Não');
+    cy.get('.formRight > :nth-child(4) > .selectsLargest').select('Nunca fiz');
+    cy.get(':nth-child(5) > .selectsLargest').select('Ansiedade');
+    cy.get(':nth-child(5) > .arrowButton').click();
+    cy.get(':nth-child(4) > input').type('Arnaldo');
+    cy.get(':nth-child(8) > input').type('61991067698');
+    cy.get(':nth-child(12) > input').type('Primo');
+    cy.get('#page3 > .form > .formRight > :nth-child(1) > .input > input').type('Marlene Aparecida');
+    cy.get('.formRight > :nth-child(2) > .input > input').type('Antonio Rodrigues');
+    cy.get('#page3 > .form > .formRight > :nth-child(3) > .input > input').type('61991235124');
+    cy.get(':nth-child(5) > .arrowButton').click();
+
+    cy.get('.button-salvar').click();
+
+    cy.wait(2000);
+  });
+
+  it("should return error when inputs are wrong", () => {
+    cy.route({
+      method: "PUT",
+      url: "**/user/**",
+      status: 203,
+      response: {
+        value: {
+          name: "user",
+          lastName: "user",
+          email: "user@esaude.com",
+          password: "teste",
+        },
+        error: {
+          _original: {
+            name: "user",
+            lastName: "user",
+            email: "user@esaude.com",
+            password: "teste",
+          },
+          details: [
+            {
+              path: ["name"],
+            },
+            {
+              path: ["lastName"],
+            },
+            {
+              path: ["email"],
+            },
+            {
+              path: ["phone"],
+            },
+            {
+              path: ["unbRegistration"],
+            },
+            {
+              path: ["gender"],
+            },
+            {
+              path: ["bond"],
+            },
+            {
+              path: ["civilStatus"],
+            },
+            {
+              path: ["race"],
+            },
+            {
+              path: ["sexualOrientation"],
+            },
+            {
+              path: ["emergencyContactName"],
+            },
+            {
+              path: ["emergencyContactPhone"],
+            },
+            {
+              path: ["emergencyContactBond"],
+            },
+            {
+              path: ["motherName"],
+            },
+            {
+              path: ["fatherName"],
+            },
+            {
+              path: ["psychiatricFollowUp"],
+            },
+            {
+              path: ["mainComplaint"],
+            },
+          ],
+        },
+      },
+    });
+
+    cy.loginPatient();
+
+    cy.get('#page1 > .form > .formRight > :nth-child(1) > .input > input').type('160128013');
+    cy.get('#page1 > .form > .formRight > :nth-child(3) > .input > input').type('61991257884');
+    cy.get(':nth-child(5) > .arrowButton').click();
+    cy.get('.formLeft > :nth-child(1) > .selectsLargest').select('Não possuo filha(o)/filhas(os)');
+    cy.get('.formLeft > :nth-child(2) > .selectsLargest').select('Solteiro');
+    cy.get('.formLeft > :nth-child(3) > .selectsLargest').select('Parda(o)');
+    cy.get('.formLeft > :nth-child(4) > .selectsLargest').select('Não');
+    cy.get(':nth-child(5) > .input > input').type('Não');
+    cy.get('.formRight > :nth-child(1) > .selectsLargest').select('Homem');
+    cy.get('#page2 > .form > .formRight > :nth-child(2) > .selectsLargest').select('Heterossexual');
+    cy.get('.formRight > :nth-child(3) > .selectsLargest').select('Não');
+    cy.get('.formRight > :nth-child(4) > .selectsLargest').select('Nunca fiz');
+    cy.get(':nth-child(5) > .selectsLargest').select('Ansiedade');
+    cy.get(':nth-child(5) > .arrowButton').click();
+    cy.get(':nth-child(4) > input').type('Arnaldo');
+    cy.get(':nth-child(8) > input').type('61991067698');
+    cy.get(':nth-child(12) > input').type('Primo');
+    cy.get('#page3 > .form > .formRight > :nth-child(1) > .input > input').type('Marlene Aparecida');
+    cy.get('.formRight > :nth-child(2) > .input > input').type('Antonio Rodrigues');
+    cy.get('#page3 > .form > .formRight > :nth-child(3) > .input > input').type('61991235124');
+    cy.get(':nth-child(5) > .arrowButton').click();
+
+    cy.get('.button-salvar').click();
+
+    cy.wait(2000);
+  });
+
+  it("should render userSchedule", () => {
+    cy.loginPatient();
+    cy.visit('/main');
+    cy.get('.closebtn').click();
+    cy.get('#openbtn').click();
+  });
 });
