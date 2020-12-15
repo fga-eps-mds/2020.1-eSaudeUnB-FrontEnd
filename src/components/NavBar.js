8b6c1f4b278ce1ac973f0b23075f2f374e75f56d
Daniel Goncalves <danmke@hotmail.com>
2020-12-14 23:02:05 -0300

    fix: fixing name
    Signed-off-by: Daniel Goncalves <danmke@hotmail.com>

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -86,7 +86,7 @@ export default function NavBar({ bond }) {
                                     pathname: '/psychologist/list',
                                 }}
                             >
-                                    Lista de Professionais
+                                    Lista de Profissionais
                             </Link>
                             <Link
                                 className="a"
